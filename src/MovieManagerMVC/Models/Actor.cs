84bef2467d312ce3cab251d305943a800ed517db
James497 <jammyhem@gmail.com>
2023-03-12 22:03:03 +0530

    added actor model

---
+++ src/MovieManagerMVC/Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieManagerMVC.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int ActorId { get; set; }
+        public string ProfilePicture { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
