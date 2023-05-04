43c895ac717ae1d568cf8b7f4031af15d945364d
James497 <jamesehembrom@gmail.com>
2023-05-04 15:51:18 +0530

    navbar design changes

--- src/MovieManagerMVC/Controllers/MoviesController.cs
+++ src/MovieManagerMVC/Controllers/MoviesController.cs
@@ -14,7 +14,7 @@ namespace MovieManagerMVC.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var allMovies = await _context.Movies.ToListAsync();
+            var allMovies = await _context.Movies.Include(n => n.Cinema).OrderBy(x => x.Name).ToListAsync();
             return View(allMovies);
         }
     }
--- src/MovieManagerMVC/Program.cs
+++ src/MovieManagerMVC/Program.cs
@@ -20,7 +20,7 @@ namespace MovieManagerMVC
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -34,7 +34,7 @@ namespace MovieManagerMVC
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Movies}/{action=Index}/{id?}");
 
             AppDbInitializer.Seed(app);
 
