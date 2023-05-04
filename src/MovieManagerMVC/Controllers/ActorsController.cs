066869487f9982a6007e6e647eb95d38de1ee79a
James497 <jamesehembrom@gmail.com>
2023-05-04 12:37:22 +0530

    added actors controller

---
+++ src/MovieManagerMVC/Controllers/ActorsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieManagerMVC.Data;
+
+namespace MovieManagerMVC.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
