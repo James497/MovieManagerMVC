5c2b9c2f7a0ab34aa96d7d5dfde22b9e564e5b13
James497 <jamesehembrom@gmail.com>
2023-05-05 12:21:33 +0530

    added entity base and base repository

--- src/MovieManagerMVC/Models/Actor.cs
+++ src/MovieManagerMVC/Models/Actor.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MovieManagerMVC.Data.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieManagerMVC.Models
 {
-    public class Actor
+    public class Actor : IEntityBase
     {
         [Key]
         public int Id { get; set; }
--- src/MovieManagerMVC/Models/Producer.cs
+++ src/MovieManagerMVC/Models/Producer.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MovieManagerMVC.Data.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieManagerMVC.Models
 {
-    public class Producer
+    public class Producer : IEntityBase
     {
         [Key]
         public int Id { get; set; }
